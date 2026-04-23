3d3306bb5f4b4d9f637d623af1f294bd11cffa15
mon <amrin0616@gmail.com>
2026-04-23 17:48:32 +0700

    feat: adjust font size for label in FactorList component

--- src/components/result/FactorList.jsx
+++ src/components/result/FactorList.jsx
@@ -86,7 +86,7 @@ export default function FactorList({ trace, answers }) {
             <div className="flex justify-between items-center">
 
               <div className="flex items-center gap-2">
-                <span className="font-medium">
+                <span className="font-medium text-base">
                   {meta.label}
                 </span>
               </div>
